6aa3ae8ad753947b3f37098b4a616ca0aa7b2448
battlegroundgametech-a11y <battlegroundgametech@gmail.com>
2026-06-19 16:55:40 +0530

    Create providers.tsx

---
+++ app/providers.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  RainbowKitProvider,
+  getDefaultConfig
+} from "@rainbow-me/rainbowkit";
+
+import {
+  WagmiProvider
+} from "wagmi";
+
+import {
+  QueryClient,
+  QueryClientProvider
+} from "@tanstack/react-query";
+
+import { sepolia } from "wagmi/chains";
+
+const config = getDefaultConfig({
+  appName: "Viltrum Card",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+  chains: [sepolia]
+});
+
+const queryClient = new QueryClient();
+
+export default function Providers({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
