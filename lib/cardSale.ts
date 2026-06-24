708d883723af0c50d6701762602646feae95108a
battlegroundgametech-a11y <battlegroundgametech@gmail.com>
2026-06-24 11:42:24 +0530

    Update cardSale.ts

--- lib/cardSale.ts
+++ lib/cardSale.ts
@@ -1,36 +1,50 @@
 export const CARD_SALE_ADDRESS =
   "0xB44325fC34Bf1679bFd5fba043933c122a44cf15";
 
-export const CARD_SALE_ABI = [
+export const CARD_SALE_ABI: any = [
   {
-    "inputs":[
-      {"internalType":"uint256","name":"amount","type":"uint256"},
-      {"internalType":"string","name":"couponCode","type":"string"}
+    inputs: [
+      { internalType: "uint256", name: "amount", type: "uint256" },
+      { internalType: "string", name: "couponCode", type: "string" }
     ],
-    "name":"purchaseVirtual",
-    "outputs":[],
-    "stateMutability":"payable",
-    "type":"function"
+    name: "purchaseVirtual",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function"
   },
   {
-    "inputs":[
-      {"internalType":"uint256","name":"amount","type":"uint256"},
-      {"internalType":"string","name":"couponCode","type":"string"}
+    inputs: [
+      { internalType: "uint256", name: "amount", type: "uint256" },
+      { internalType: "string", name: "couponCode", type: "string" }
     ],
-    "name":"purchasePhysical",
-    "outputs":[],
-    "stateMutability":"payable",
-    "type":"function"
+    name: "purchasePhysical",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function"
   },
   {
-    "inputs":[
-      {"internalType":"uint8","name":"cardType","type":"uint8"},
-      {"internalType":"uint256","name":"amount","type":"uint256"},
-      {"internalType":"string","name":"couponCode","type":"string"}
+    inputs: [
+      { internalType: "uint8", name: "cardType", type: "uint8" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
+      { internalType: "string", name: "couponCode", type: "string" }
     ],
-    "name":"getFinalPrice",
-    "outputs":[{"internalType":"uint256","name":"","type":"uint256"}],
-    "stateMutability":"view",
-    "type":"function"
+    name: "getFinalPrice",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "virtualPrice",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "physicalPrice",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function"
   }
-] as const;
+];
